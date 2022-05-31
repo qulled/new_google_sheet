b2be889f5ba802297b4ba2f674cb671b926da5ca
qulled <76214223+qulled@users.noreply.github.com>
2022-05-31 14:37:56 +0300

    release

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 from googleapiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials
 import os
-from dotenv import load_dotenv
+import json
 
 import datetime
+from dateutil import relativedelta
 
 d = datetime.date.today()
+year = d.year
+nextmonth = datetime.date.today() + relativedelta.relativedelta(months=1)
 
-CREDENTIALS_FILE = 'credentials.json'
-
+CREDENTIALS_FILE = 'credentials_service.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
                                                                ['https://www.googleapis.com/auth/spreadsheets',
                                                                 'https://www.googleapis.com/auth/drive'])
 service = discovery.build('sheets', 'v4', credentials=credentials)
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-spreadsheet_id = os.environ['SPREADSHEET_ID']
+'''Создание копии рабочего листа следующего за текущим месяца.
+   Автозагруска скрипта на сервере должна происходить в первый день месяца в 00ч:00м:00с.'''
 
 
-# создание копии рабочего листа прошедшего месяца
 def copy_sheets(table_id):
+    if nextmonth.strftime("%m") == '01':
+        year = d.year + 1
     requests = {
         'duplicateSheet': {
             'sourceSheetId': 0,
             'insertSheetIndex': 1,
-            'newSheetId': f'{d.year}{d.month }',
-            'newSheetName': f'{d.month }.{d.year}'
+            # 'newSheetId': f'{d.year}{d.strftime("%m")}',
+            'newSheetName': f'{nextmonth.strftime("%m")}.{year}'
         }
     }
     body = {
@@ -39,19 +41,45 @@ def copy_sheets(table_id):
     return response
 
 
-# очистка полей данных и дат у рабочего листа
+'''Очистка полей данных и дат у рабочего листа.
+   Удаление столбцов цен и статистики продаж, за исключением основных данных и столюца остатков.'''
+
+
 def clear_sheets_data(table_id):
     batch_clear_values_request_body = {
-        'ranges': [['J3:ZZ999'], ['O2:ZZ2']],
+        'ranges': [['J3:J999'], ['L3:ZZ999'], ['O2:ZZ2']],
     }
     response = service.spreadsheets().values().batchClear(spreadsheetId=table_id,
                                                           body=batch_clear_values_request_body).execute()
     return response
 
 
+'''Создание новго листа с нужным айди.'''
+
+
+def new_sheet(table_id):
+    requests = {
+        'addSheet': {
+            "properties": {
+                'sheetId': 11,
+                'title': 'созданный лист'
+            }
+        }
+    }
+    body = {
+        'requests': requests
+    }
+    response = service.spreadsheets().batchUpdate(spreadsheetId=table_id,
+                                                  body=body).execute()
+    return response
+
+
 if __name__ == '__main__':
-    copy_sheets(spreadsheet_id)
-    clear_sheets_data(spreadsheet_id)
-
-
-
+    cred_file = os.path.join(BASE_DIR, 'credentials.json')
+    with open(cred_file, 'r') as f:
+        cred = json.load(f)
+    for i in cred:
+        table_id = cred[i].get('table_id')
+        copy_sheets(table_id)
+#    clear_sheets_data(table_id)
+#    new_sheet(table_id)
